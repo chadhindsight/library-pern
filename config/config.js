cf75a8ddddccbadd71cc55bd2cdcbcb7ad0fe6e4
chad hinds <chadrickj8@gmail.com>
2024-11-21 13:08:02 -0500

    Use env variables

--- config/config.js
+++ config/config.js
@@ -1,23 +1,24 @@
 module.exports = {
   development: {
-    username: "root",
-    password: null,
-    database: "database_development",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.BD_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    logging: true,
+    dialect: "postgres"
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.BD_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.BD_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
   }
 };
