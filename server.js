0d0e6f93d5f0012978faebfe27ca5252d2254eaa
chad hinds <chadrickj8@gmail.com>
2024-11-21 14:30:15 -0500

    add Sequelizerc and migrations

---
+++ server.js
@@ -0,0 +1,16 @@
+// Required dependencies
+const express = require('express');
+const app = express();
+const { Sequelize } = require('sequelize');  // Added space around '=' for consistency
+const path = require('path');
+const cors = require('cors');
+
+// Configuration
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(7005, () => {
+    console.log('Server is running on port 7005')
+})
